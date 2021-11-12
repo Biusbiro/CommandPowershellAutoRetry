22b7019254d7853262485d2af3e572638f583d18
Bismarck Souza <C.BISMARCK.SOUZA@kmaxx.com.br>
2021-11-12 14:16:39 -0300

    Initial Commit

---
+++ ExecuteCommands/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace ExecuteCommands
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.grdResults = new System.Windows.Forms.DataGridView();
+            this.result = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.txtCommand = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.grdResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdResults
+            // 
+            this.grdResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdResults.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.result});
+            this.grdResults.Location = new System.Drawing.Point(1, 0);
+            this.grdResults.Name = "grdResults";
+            this.grdResults.RowTemplate.Height = 25;
+            this.grdResults.Size = new System.Drawing.Size(775, 514);
+            this.grdResults.TabIndex = 0;
+            // 
+            // result
+            // 
+            this.result.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.result.HeaderText = "Column1";
+            this.result.Name = "result";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(695, 520);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(70, 50);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "RUN";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // txtCommand
+            // 
+            this.txtCommand.Location = new System.Drawing.Point(75, 521);
+            this.txtCommand.Name = "txtCommand";
+            this.txtCommand.Size = new System.Drawing.Size(618, 23);
+            this.txtCommand.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 525);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Command";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 550);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Interval";
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.Location = new System.Drawing.Point(75, 547);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(57, 23);
+            this.txtInterval.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 586);
+            this.Controls.Add(this.txtInterval);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCommand);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.grdResults);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grdResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdResults;
+        private System.Windows.Forms.DataGridViewTextBoxColumn result;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox txtCommand;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInterval;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ExecuteCommands/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExecuteCommands
+{
+    public partial class Form1 : Form
+    {
+        Boolean isActive;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            isActive = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isActive)
+            {
+                grdResults.Rows.Add("Teste");
+            }
+        }
+    }
+}
